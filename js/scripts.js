f5ef76a9b29621a0b9079e264b013cc7eea3d64a
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-01-20 18:28:26 -0300

    finish create page, refactoring and init update page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 class Product {
-     constructor(name, cateogry, price) {
+     constructor(name, category, price) {
           this.id = utils.randomNumber();
           this.name = name;
-          this.category = cateogry;
+          this.category = category;
           this.price = price;
           this.isValid = false;
           this.notifications = [];
@@ -42,6 +42,9 @@ class ProductRepository {
      set(products) {
           localStorage.setItem(this.localStorageKey, JSON.stringify(products));
      }
+     removeAll() {
+          localStorage.removeItem(this.localStorageKey);
+     }
 }
 
 class ProductService {
@@ -53,6 +56,27 @@ class ProductService {
           let products = this.repository.get();
 
           products.push(product);
+
+          this.repository.set(products);
+     }
+
+     update(product) {
+          debugger;
+          const { id, name, category, price } = product;
+
+          let products = this.repository.get();
+
+          console.log(products);
+
+          let productToUpdate = products.filter(x => x.id !== id);
+
+          console.log(products);
+
+          productToUpdate.name = name;
+          productToUpdate.category = category;
+          productToUpdate.price = price;
+
+          products.push(productToUpdate);
 
           this.repository.set(products);
      }
@@ -104,6 +128,46 @@ const operations = {
           one: (id) => repository.get().find(x => x.id == id)
 
      },
-     update: () => { },
+     update: (id, name, category, price) => {
+          let product = new Product(name, category, price);
+
+          product.id = id;
+
+          console.log(product)
+
+          if (!product.isValid) {
+               return product;
+          }
+
+          service.update(product);
+
+          return product;
+     },
      delete: () => { }
+}
+
+const notificationHelper = {
+     resetNotifications: () => {
+          const ids = [
+               "name",
+               "category",
+               "price"
+          ];
+
+          ids.forEach(x => {
+               let divOfNotification = document.getElementById(`${x}-notification`);
+
+               divOfNotification.innerText = '';
+          })
+
+     },
+     showNotifications: (notifications) => {
+          console.log(notifications);
+
+          notifications.forEach(n => {
+               let divOfNotification = document.getElementById(`${n.key}-notification`);
+
+               divOfNotification.innerText = n.message;
+          });
+     }
 }
